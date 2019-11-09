287b36b0d18019188a28f2f99eafa1b696bfa37d
YuvalP <ypodoksik29@gmail.com>
2019-11-09 16:01:11 +0200

    k8s automation fix

--- scripts/k8s_Deploy_To_Production.sh
+++ scripts/k8s_Deploy_To_Production.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 kubectl set image deployment/production-app docker-production-app=yp29/jenkinsmultibranch:production-base
-sleep 12 # waiting before deleteing the unused latest image localy
+sleep 45 # waiting before deleteing the unused latest image localy
 docker image prune -af
 kubectl set image deployment/production-app docker-production-app=yp29/jenkinsmultibranch:production-latest
-sleep 12 # waiting before deleteing the unused base image localy
+sleep 45 # waiting before deleteing the unused base image localy
 docker image prune -af
